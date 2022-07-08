1b21f5661726a904899c81a9e90186aee275bfc3
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-08 18:08:39 +0300

    Added new project

---
+++ Inheritance-Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0 ? true : false;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> elements)
+        {
+            foreach (string element in elements)
+            {
+                this.Push(element);
+            }
+            return this;
+        }
+    }
+}
