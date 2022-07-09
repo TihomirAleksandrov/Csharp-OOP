29c8fe92822f608e9ff4c46f48165b764be2e46c
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-09 15:22:13 +0300

    Added new project

---
+++ Inheritance-Exercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            this.DefaultFuelConsumption = 8;
+        }
+
+        public override double FuelConsumption
+        {
+            get
+            {
+                return DefaultFuelConsumption;
+            }
+        }
+    }
+}
