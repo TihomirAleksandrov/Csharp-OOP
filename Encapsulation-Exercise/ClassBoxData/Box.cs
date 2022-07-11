2dafeebf7ebba21469de8d3711a875a294fcddd1
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-11 18:06:59 +0300

    Added new project

---
+++ Encapsulation-Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length
+        {
+            get { return this.length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2*Length*Width + 2*Length*Height + 2*Width*Height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.Append($"Volume - {Volume():f2}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Encapsulation-Exercise/ClassBoxData/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box;
+            try
+            {
+                box = new Box(length, width, height);
+            }
+            catch (ArgumentException exeption)
+            {
+                Console.WriteLine(exeption.Message);
+                return;
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
