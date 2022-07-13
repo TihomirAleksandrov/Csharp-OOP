02635a07e04a4d406d22e671bf728b16296d40ff
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-14 00:11:23 +0300

    Added new project

---
+++ InterfacesAndAbstraction-Lab/Cars/Seat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public string Start()
+        {
+            return "Engine Start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.color} {GetType().Name} {this.model}";
+        }
+    }
+}
