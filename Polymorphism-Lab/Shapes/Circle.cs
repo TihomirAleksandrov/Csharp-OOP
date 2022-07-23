daad92426fc397a65893a9208bdd7e71a59fecf5
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-23 14:46:45 +0300

    Added new project

---
+++ Polymorphism-Lab/Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get => radius;
+            private set => radius = value;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Math.PI * Radius);
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + $"{this.GetType().Name}";
+        }
+    }
+}
---
+++ Polymorphism-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get => height;
+            private set => height = value;
+        }
+
+        public double Width
+        {
+            get => width;
+            private set => width = value;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (Height + Width) * 2;
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + $"{this.GetType().Name}";
+        }
+    }
+}
