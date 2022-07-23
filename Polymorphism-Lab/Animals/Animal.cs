a875908ae38d15cfeb587b1e1cf4ee06f788cc48
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-07-23 14:09:41 +0300

    Added new project

---
+++ Polymorphism-Lab/Animals/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavoriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set => name = value;
+        }
+
+        public string FavoriteFood
+        {
+            get => favouriteFood;
+            private set => favouriteFood = value;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavoriteFood}";
+        }
+    }
+}
