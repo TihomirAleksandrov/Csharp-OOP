40fa2c6c7fb17d99380ff8c798ae000d0cd4de51
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-08-05 00:55:17 +0300

    Added more tests

--- UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
+++ UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,36 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void DummyLoosesHealthIfAttacked()
         {
-
+            Dummy dummy = new Dummy(10, 10);
+            dummy.TakeAttack(5);
+            
+            Assert.AreEqual(5, dummy.Health);
         }
-    }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.That(() => dummy.TakeAttack(5), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.AreEqual(10, dummy.GiveExperience());
+        }
+
+        [Test]
+        public void AliveDummyCanNotGiveXP()
+        {
+            Dummy dummy = new Dummy(10, 10);
+
+            Assert.That(() => dummy.GiveExperience(), Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+        }
+}
 }
